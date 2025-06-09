22f6f6b75c9cfe90625bc763e6ee1c5876cd1ead
jordany22 <jimekaneki@gmail.com>
2025-06-08 21:47:15 -0400

    ejercicio5

---
+++ pratica1ejercicio5/ejercicio5/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ejercicio5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.btnAcertar = new System.Windows.Forms.Button();
+            this.lblMostrarOpcion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(185, 52);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Opcion 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(354, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Opcion 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(567, 52);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Opcion 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // btnAcertar
+            // 
+            this.btnAcertar.Location = new System.Drawing.Point(607, 265);
+            this.btnAcertar.Name = "btnAcertar";
+            this.btnAcertar.Size = new System.Drawing.Size(75, 23);
+            this.btnAcertar.TabIndex = 3;
+            this.btnAcertar.Text = "Acertar";
+            this.btnAcertar.UseVisualStyleBackColor = true;
+            this.btnAcertar.Click += new System.EventHandler(this.btnAcertar_Click);
+            // 
+            // lblMostrarOpcion
+            // 
+            this.lblMostrarOpcion.AutoSize = true;
+            this.lblMostrarOpcion.Location = new System.Drawing.Point(212, 256);
+            this.lblMostrarOpcion.Name = "lblMostrarOpcion";
+            this.lblMostrarOpcion.Size = new System.Drawing.Size(35, 13);
+            this.lblMostrarOpcion.TabIndex = 4;
+            this.lblMostrarOpcion.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMostrarOpcion);
+            this.Controls.Add(this.btnAcertar);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button btnAcertar;
+        private System.Windows.Forms.Label lblMostrarOpcion;
+    }
+}
+
---
+++ pratica1ejercicio5/ejercicio5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcertar_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                lblMostrarOpcion.Text = "selecionaste: Opcion 1";
+            else if (radioButton2.Checked)
+                lblMostrarOpcion.Text = "selecionaste: Opcion 2";
+            else if (radioButton3.Checked)
+                lblMostrarOpcion.Text = "selecionaste: Opcion 3";
+            else
+                lblMostrarOpcion.Text = "No selecionaste niguna Opcion";
+                
+
+        }
+    }
+}
