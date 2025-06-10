25fc172f86db3ec66eb80f491e7c2fb533880435
jordany22 <jimekaneki@gmail.com>
2025-06-09 22:33:26 -0400

    ejercicio6

---
+++ pratica1ejercicio6/ejercicio6/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if
+                (comboBox1.SelectedItem.ToString() ==
+                "ROJO")
+                this.BackColor = Color.Red;
+            else if
+                (comboBox1.SelectedItem.ToString() ==
+                "verde")
+                this.BackColor = Color.Green;
+            else if
+                (comboBox1.SelectedItem.ToString() == 
+                "Azul")
+                this.BackColor = Color.Blue;
+        }
+    }
+}
