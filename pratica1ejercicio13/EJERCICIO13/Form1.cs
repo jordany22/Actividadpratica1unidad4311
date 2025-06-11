ae75c4cb0e1b6f14343f1f8e25e520f8ad19331a
jordany22 <jimekaneki@gmail.com>
2025-06-10 23:13:26 -0400

    ejercicio14

---
+++ pratica1ejercicio13/EJERCICIO13/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInicial_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            timer1.Start();
+        }
+
+        private void BtnDetener_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+                progressBar1.Value += 1;
+            else
+                timer1.Stop();
+        }
+    }
+}
