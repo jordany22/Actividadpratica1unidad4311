4d7793b8fd5a06ac6b2383b89c79e4be56149df8
jordany22 <jimekaneki@gmail.com>
2025-06-10 22:51:38 -0400

    EJERCICIO12

---
+++ pratica1ejercicio12/ejercicio12/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCovertir_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double celsius))
+            {
+                double fahernhit = (celsius * 9 / 5) + 32;
+
+
+                lblResultado.Text = $"fahrenhit: {fahernhit}";
+
+                
+            }
+        }
+    }
+
+}
+
+
+
