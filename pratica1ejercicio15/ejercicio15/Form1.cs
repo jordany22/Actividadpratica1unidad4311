00f7ca6a8618771c0dec3f1af51a908a4bc9b963
jordany22 <jimekaneki@gmail.com>
2025-06-10 23:33:26 -0400

    ejercicio5

---
+++ pratica1ejercicio15/ejercicio15/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAbriel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK) 
+            {
+
+                textBox1.Text = openFileDialog.FileName;
+        }
+    }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) 
+            
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+        }
+    }
+}
+}
+
+
